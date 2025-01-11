099303897ae83ef7462e4c6a07c9e3e05fff6b06
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-01-11 16:12:38 +0530

    feat: :construction: Accomplishments crud and some other small changes
    just crud for Accomplishments, and some other small changes

---
+++ db/crudAccomplishments.ts
@@ -0,0 +1,71 @@
+import { Accomplishments } from "@prisma/client";
+import { prismaClient } from "./client";
+
+export const getAllAccomplishments = async () => {
+  try {
+    const allAccomplishments = await prismaClient.accomplishments.findMany();
+    console.log(`successfully got all accomplishments: ${allAccomplishments}`);
+    return { success: true, data: allAccomplishments };
+  } catch (error) {
+    console.error(error);
+    return { success: false, data: error };
+  }
+};
+
+export const getAccomplishmentsByUserId = async (userId: string) => {
+  try {
+    const accomplishments = await prismaClient.accomplishments.findMany({
+      where: { userId },
+    });
+    console.log(`successfully got accomplishments: ${accomplishments}`);
+    return { success: true, data: accomplishments };
+  } catch (error) {
+    console.error(error);
+    return { success: false, data: error };
+  }
+};
+
+export const createAccomplishment = async (data: Accomplishments) => {
+  try {
+    const accomplishment = await prismaClient.accomplishments.create({ data });
+    console.log(`successfully created accomplishment: ${accomplishment}`);
+    return { success: true, data: accomplishment };
+  } catch (error) {
+    console.error(error);
+    return { success: false, data: error };
+  }
+};
+
+export const updateAccomplishment = async (
+  id: string,
+  data: Accomplishments
+) => {
+  try {
+    const updatedAccomplishment = await prismaClient.accomplishments.update({
+      where: { id },
+      data,
+    });
+    console.log(
+      `successfully updated accomplishment: ${updatedAccomplishment}`
+    );
+    return { success: true, data: updatedAccomplishment };
+  } catch (error) {
+    console.error(error);
+    return { success: false, data: error };
+  }
+};
+
+export const deleteAccomplishment = async (id: string) => {
+  try {
+    const deletedAccomplishment = await prismaClient.accomplishments.delete({
+      where: { id },
+    });
+    console.log(
+      `successfully deleted accomplishment: ${deletedAccomplishment} `
+    );
+    return { success: true, data: deletedAccomplishment };
+  } catch (error) {
+    console.error(error);
+    return { success: false, data: error };
+  }
+};
--- db/crudUser.ts
+++ db/crudUser.ts
@@ -1,4 +1,5 @@
 "use server";
+
 import { User } from "@prisma/client";
 import { prismaClient } from "./client";
 
@@ -6,7 +7,7 @@ export const createUser = async (data: User) => {
   try {
     const creationStatement = await prismaClient.user.create({ data });
     console.log(`successfully created user: ${creationStatement}`);
-    return { success: true, data: creationStatement };
+    return { success: true, data: creationStatement as User };
   } catch (error) {
     console.error(error);
     return { success: false, data: error };
@@ -17,7 +18,7 @@ export const getAllUsers = async () => {
   try {
     const allUsers = await prismaClient.user.findMany();
     console.log(`successfully got all users: ${allUsers}`);
-    return { success: true, data: allUsers };
+    return { success: true, data: allUsers as User[] };
   } catch (error) {
     console.error(error);
     return { success: false, data: error };
@@ -28,7 +29,7 @@ export const getUserById = async (id: string) => {
   try {
     const user = await prismaClient.user.findUnique({ where: { id } });
     console.log(`successfully got user: ${user}`);
-    return { success: true, data: user };
+    return { success: true, data: user as User };
   } catch (error) {
     console.error(error);
     return { success: false, data: error };
@@ -39,7 +40,7 @@ export const updateUser = async (id: string, data: User) => {
   try {
     const updatedUser = await prismaClient.user.update({ where: { id }, data });
     console.log(`successfully updated user: ${updatedUser}`);
-    return { success: true, data: updatedUser };
+    return { success: true, data: updatedUser as User };
   } catch (error) {
     console.error(error);
     return { success: false, data: error };
@@ -50,7 +51,7 @@ export const deleteUser = async (id: string) => {
   try {
     const deletedUser = await prismaClient.user.delete({ where: { id } });
     console.log(`successfully deleted user: ${deletedUser}`);
-    return { success: true, data: deletedUser };
+    return { success: true, data: deletedUser as User };
   } catch (error) {
     console.error(error);
     return { success: false, data: error };
